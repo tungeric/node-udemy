ae8ab9c1bc894b50cf2d8689600f218f6b04cc83
Eric Tung <eric.p.tung@gmail.com>
2017-12-11 13:49:18 -0800

    Cover process.argv

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,29 +2,22 @@ console.log('Starting app.js');
 
 // open up https://nodejs.org/api and grab some modules
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString("Ben Simmons"));
-const filteredArray = _.uniq(['Embiid', 1, "Embiid", 1,2,3,4]);
-console.log(filteredArray);
+let command = process.argv[2];
+console.log('Command:', command);
+console.log(process.argv);
 
-// let res = notes.addNote();
-// console.log(res);
-
-// let sum = notes.add(2,3);
-// console.log(sum);
-
-// let user = os.userInfo();
-// console.log(user);
-
-// fs.appendFile('greetings.txt', `Trust the process, ${ user.username }! You are ${ notes.age }`);
-
-// //does the same thing
-// fs.appendFild('greetings.txt', 'Trust the process', function(err) {
-//   if(err) {
-//     console.log('unable to write to file');
-//   }
-// };
+if(command === 'add') {
+  console.log('Adding new note');
+} else if (command === 'list') {
+  console.log('Listing all notes');
+} else if (command === 'read') {
+  console.log('Reading a note');
+} else if (command === 'remove') {
+  console.log('Removing the note');
+} else {
+  console.log('Command not recognized');
+}

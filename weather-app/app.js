f3088d781479ca6cf0e7a97255370f3f75797435
Eric Tung <eric.p.tung@gmail.com>
2017-12-11 18:05:32 -0800

    Refactor and reformat weather app to make more dry

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,7 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
 const argv = yargs
   .options({
     a:{
@@ -16,23 +17,10 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-// console.log(argv);
-
-const encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to Google servers');
-  } else if (body.status === 'OK') {
-    // console.log(JSON.stringify(response, undefined, 2));
-    // console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find address');
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if(errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
   }
 });

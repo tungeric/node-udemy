82e062c8d8ba6f1a14b267b936f1886ea5d28244
Eric Tung <eric.p.tung@gmail.com>
2017-12-13 13:35:59 -0800

    Integrate handlebars

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const hbs = require ('hbs');
 
 const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 // route handlers
 app.get('/', (req, res) => {
   // res.send('<h1>Trust the process, Express!</h1>');
-  res.send({
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Trust the Process!',
     name: 'Sam Hinkie',
     likes: [
       'Processing',
@@ -16,7 +31,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Sam');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
 app.get('/bad', (req, res) => {
@@ -28,4 +45,6 @@ app.get('/bad', (req, res) => {
 });
 
 // tell the app to listen
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});

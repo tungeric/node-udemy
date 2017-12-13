16bd147981a3c26e1348127898914f507915738b
Eric Tung <eric.p.tung@gmail.com>
2017-12-12 17:32:39 -0800

    Cover promises

--- notes-node/playground/playground.js
+++
@@ -1,9 +0,0 @@
-const somePromise = new Promise((resolve, reject) => {
-  resolve('Hey it worked');
-});
-
-somePromise.then((message) => {
-  console.log('Success: ', message);
-}, (errorMessage) => {
-  console.log('Error: ', errorMessage)
-});
---
+++ weather-app/playground/promise.js
@@ -0,0 +1,32 @@
+const asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else{
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
+
+asyncAdd(5,7).then((result) => {
+  console.log('Result: ', result);
+  return asyncAdd(result, 33);
+}).then((result) => {
+  console.log('Should be 45', result);
+}).catch((errorMessage) => {
+  console.log(errorMessage);
+});
+
+const somePromise = new Promise((resolve, reject) => {
+  setTimeout(() =>{
+    resolve('Hey it worked');
+  }, 2500);
+});
+
+somePromise.then((message) => {
+  console.log('Success: ', message);
+}, (errorMessage) => {
+  console.log('Error: ', errorMessage);
+});
